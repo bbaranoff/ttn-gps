eb36e79a8fd75447bde4c1d4cb55337c9800f79a
bbaranoff <37385191+bbaranoff@users.noreply.github.com>
2021-11-03 13:41:10 +0100

    Update dragino-rpi4-tracker.sh

--- dragino-rpi4-tracker.sh
+++ dragino-rpi4-tracker.sh
@@ -4,6 +4,7 @@ sudo apt install git device-tree-compiler git python3-crypto python3-nmea2 pytho
 gcc coordinates.c -o coordinates -lm -lgps
 sudo cp config.txt /boot/config.txt
 sudo cp cmdline.txt /boot/cmdline.txt
+sudo cp gpsd /etc/default/gpsd
 cd dragino/overlay
 sudo dtc -@ -I dts -O dtb -o spi-gpio-cs.dtbo spi-gpio-cs-overlay.dts
 sudo cp spi-gpio-cs.dtbo /boot/overlays/

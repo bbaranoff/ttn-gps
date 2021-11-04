916e74cae1add55c810d037d1f7499069a8d3f60
bbaranoff <37385191+bbaranoff@users.noreply.github.com>
2021-11-04 10:10:05 +0100

    Update dragino-rpi4-tracker.sh

--- dragino-rpi4-tracker.sh
+++ dragino-rpi4-tracker.sh
@@ -2,7 +2,7 @@
 sudo apt update && sudo apt upgrade
 sudo apt install git device-tree-compiler git python3-crypto python3-nmea2 python3-rpi.gpio python3-serial python3-spidev python3-configobj gpsd libgps-dev gpsd-clients libgps23
 gcc coordinates.c -o coordinates -lm -lgps
-cp coordinates ..
+cp coordinates /home/pi/coordinates
 sudo cp config.txt /boot/config.txt
 sudo cp cmdline.txt /boot/cmdline.txt
 sudo cp gpsd /etc/default/gpsd
